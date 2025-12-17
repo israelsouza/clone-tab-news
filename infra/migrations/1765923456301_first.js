0a7b386e876bd81a2b38c62d8449988ade5afb7e
Israel de Souza <83998022+israelsouza@users.noreply.github.com>
2025-12-17 14:26:51 +0000

    melhora o script  e estabiliza ambiente local

--- infra/migrations/1765923456301_first.js
+++ infra/migrations/1765923456301_first.js
@@ -1,20 +1,18 @@
 /**
  * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
  */
-const shorthands = undefined;
+export const shorthands = undefined;
 
 /**
  * @param pgm {import('node-pg-migrate').MigrationBuilder}
  * @param run {() => void | undefined}
  * @returns {Promise<void> | void}
  */
-const up = (pgm) => {};
+export const up = (pgm) => {};
 
 /**
  * @param pgm {import('node-pg-migrate').MigrationBuilder}
  * @param run {() => void | undefined}
  * @returns {Promise<void> | void}
  */
-const down = (pgm) => {};
-
-module.exports = { shorthands, up, down };
+export const down = (pgm) => {};
