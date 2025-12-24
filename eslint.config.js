48768eced35d84e5de338d5be9e84138329dcbd1
Israel de Souza <israellimas@hotmail.com>
2025-12-24 01:02:27 -0300

    fix eslint errors + crazy all files with prettier fix

--- eslint.config.js
+++ eslint.config.js
@@ -1,27 +1,29 @@
 import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from 'eslint-config-next/core-web-vitals'
+import nextVitals from "eslint-config-next/core-web-vitals";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 export default defineConfig([
   ...nextVitals,
   eslintConfigPrettier,
-    globalIgnores([
-    '.next/**',
-    'out/**',
-    'build/**',
-    'next-env.d.ts',
-    'infra/migrations/**'
+  globalIgnores([
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+    "infra/migrations/**",
   ]),
-  { 
-    files: ["**/*.{js,mjs,cjs}"], 
-    plugins: { js }, 
-    extends: ["js/recommended"], 
-    languageOptions: { 
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
       globals: {
-        ...globals.browser, ...globals.node, ...globals.jest
-      } 
-    } 
+        ...globals.browser,
+        ...globals.node,
+        ...globals.jest,
+      },
+    },
   },
 ]);
