d4bd076870e65b329563b2818e79f3ad8d1bbb7e
Israel de Souza <israellimas@hotmail.com>
2025-12-24 00:35:19 -0300

    create script and configuration for `ESLint`

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from 'eslint-config-next/core-web-vitals'
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+
+export default defineConfig([
+  ...nextVitals,
+  eslintConfigPrettier,
+    globalIgnores([
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+    'infra/migrations/**'
+  ]),
+  { 
+    files: ["**/*.{js,mjs,cjs}"], 
+    plugins: { js }, 
+    extends: ["js/recommended"], 
+    languageOptions: { 
+      globals: {
+        ...globals.browser, ...globals.node, ...globals.jest
+      } 
+    } 
+  },
+]);
